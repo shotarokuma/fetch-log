d6166a49acf4e202bbe0455131cf38cd89268328
Shotaro Kumagai <75811152+shotarokuma@users.noreply.github.com>
2023-05-24 16:43:35 -0700

    fix my_user.c

--- my_userid.c
+++ my_userid.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 const int MAX_INPUT = 100;
+const int BUF = 256;
 
 void getInput(char uname[])
 {
@@ -11,11 +12,30 @@ void getInput(char uname[])
 
 void getUserId(char uname[])
 {
+  char res[BUF];
   char command[MAX_INPUT] = "id -u ";
   strcat(command, uname);
-  system(command);
-}
+  strcat(command, " 2>&1");
 
+  FILE *file = popen(command, "r");
+  if (file)
+  {
+    if (fgets(res, BUF, file))
+    {
+      if (res[0] != 'i')
+      {
+        int userId = atoi(res);
+        printf("%d\n", userId);
+      }
+      else
+      {
+        printf("no such user found\n");
+      }
+    }
+
+    pclose(file);
+  }
+}
 int main()
 {
   char uname[MAX_INPUT] = "";
